912d574066ce93d83365f2f4a0b96feb515fe447
Noah Beaudin <noah.beauidn1@gmail.com>
2021-10-31 09:24:12 -0600

    Setted up main bg

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 import Header from "./components/header/Header.svelte";
+import Main from "./Main.svelte";
 
 
 </script>
 
 <main>
     <Header/>
+    <Main/>
 </main>
 
 <style>
-	
+    main {
+        height: 100vh;
+    }	
 </style>
---
+++ src/Main.svelte
@@ -0,0 +1,14 @@
+<div class="main">
+
+</div>
+
+<style>
+    .main {
+        height: 100vh;
+        background-image: url("../images/wpaback.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: calc(100% - 125px);
+        width: 100%;
+    }
+</style>
