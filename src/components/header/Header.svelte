3cc4546bce5e51fec322786c1fd001da92e88da2
Noah Beaudin <noah.beauidn1@gmail.com>
2021-10-31 09:13:33 -0600

    Created the header

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,12 @@
+<div class="header">
+    <img src="images/mslogo.jpg" alt="" class="header-logo">
+</div>
+
+<style>
+    .header {
+        background-image: url("../images/header-bg.jpg");
+    }
+    .header-logo {
+        margin-left: 1.5rem;
+    }
+</style>
