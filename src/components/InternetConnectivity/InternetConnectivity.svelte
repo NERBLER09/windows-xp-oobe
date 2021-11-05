cceadb01af5bb2fffda04909dbbda355643d9de2
Noah Beaudin <noah.beauidn1@gmail.com>
2021-11-05 07:11:45 -0600

    Added the internet connectivity screen

---
+++ src/components/InternetConnectivity/InternetConnectivity.svelte
@@ -0,0 +1,29 @@
+<div class="internet-connectivity">
+    <header>
+        <h1 class="title-text">Checking your Internet connectivity</h1>
+        <p class="body-text">Please wait a moment while Windows checks to see if this computer is already connected to the Internet</p>
+
+        <div class="dialup">
+            <img src="images/dialup.gif" alt="" class="dialup-img">
+        </div>
+    </header>
+</div>
+
+<style>
+    header {
+        padding-top: 10px;
+        margin-left: 50px;
+    }
+
+    .dialup {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    p {
+        margin-top: 40px;
+        margin-bottom: 40px;
+    }
+</style>
--- src/routes/Routes.svelte
+++ src/routes/Routes.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import { Router } from "@jamen/svelte-router"
+import InternetConnectivity from "../components/InternetConnectivity/InternetConnectivity.svelte";
 import Home from "../pages/home/Home.svelte";
 import Updates from "../pages/updates/Updates.svelte";
 
     const routes = {
         "/": Home,
-        "/updates": Updates
+        "/updates": Updates,
+        "/internetConnectivity": InternetConnectivity
     }
 </script>
 
