8295b50308fc53c70c289aa202bb20856540f6b7
Noah Beaudin <noah.beauidn1@gmail.com>
2021-11-05 20:38:47 -0600

    Reorginized pages

--- src/routes/Routes.svelte
+++ src/routes/Routes.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import { Router } from "@jamen/svelte-router"
-import ConnectToInternet from "../components/connectToInternet/ConnectToInternet.svelte";
-import InternetConnectivity from "../components/InternetConnectivity/InternetConnectivity.svelte";
-import Users from "../components/users/Users.svelte";
+import ConnectToInternet from "../pages/connectToInternet/ConnectToInternet.svelte";
+import InternetConnectivity from "../pages/InternetConnectivity/InternetConnectivity.svelte";
+import Users from "../pages/users/Users.svelte";
 import Finish from "../pages/finish/Finish.svelte";
 import Home from "../pages/home/Home.svelte";
 import Updates from "../pages/updates/Updates.svelte";
